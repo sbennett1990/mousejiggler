2255baa1e420b299c3f9b09dcae958d29332e654
Scott Bennett <sbennett1990@gmail.com>
2018-03-29 13:26:55 -0400

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ saver activation, etc., is prevented, but the pointer does not actually move."
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
